7f16435d21347523e6f9ac5bc643dd55c3f0aa44
Pesach Bagad <bgdpsh@gmail.com>
2026-06-04 08:22:58 +0300

    Add logger

--- server.py
+++ server.py
@@ -17,21 +17,25 @@ def return_all_shapes():
     logger.info("enter to return_all_shapes in web server")
     result=shape_manage.get_all_shapes()
     if not result:
-        logger.exception("Errror ")
+        logger.exception("Error to find the data")
         raise HTTPException(status_code=404, detail="Shape id was not found")
     return result
 
 @app.get("/shapes/total-area")
 def sum_of_all_area_shapes():
+    logger.info("enter to sum_of_all_area_shapes in web server")
     result= shape_manage.get_sum_of_all_area()
     if not result:
+        logger.exception("Error to find the data")
         raise HTTPException(status_code=404, detail="There is not shapes to calculate area")
     return result
 
 @app.get("/shapes/{id}")
 def return_shape_by_id(id:int):
+    logger.info("enter to return_shape_by_id in web server")
     shape=shape_manage.get_shape_by_id(id)
     if not shape:
+        logger.exception("Error to find the data")
         raise HTTPException(status_code=404,detail="Shape id was not found")
     return shape
 
@@ -44,22 +48,28 @@ def return_shape_by_id(id:int):
 
 @app.delete("/shapes/{id}")
 def delete_shape(id:int):
+    logger.info("enter to delete_shapes in web server")
     deleted=shape_manage.delete_shape(id)
     if not deleted:
+        logger.exception("Error to find the data")
         raise HTTPException(status_code=404,detail="Shape id was not found")
     return deleted
 
 @app.post("/shapes")
 async def create_new_shape(data: dict):
+    logger.info("enter to create_new_shape in web server")
     result=shape_manage.create_shape_1(data)
     if not result:
+        logger.exception("Error to find the data")
         raise HTTPException(status_code=400, detail="Faild to create a new shape")
     return result
 
 @app.put("/shapes/{id}")
 def change_params(id:int,data:dict):
+     logger.info("enter to change_params in web server")
      result=shape_manage.update_shape(id,data)
      if not result:
+         logger.exception("Error to find the data")
          raise HTTPException(status_code=404, detail="Shape id was not found")
      return {'message': "updated successfully"}
 
